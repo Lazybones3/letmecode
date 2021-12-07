afa2350763b51e052b3d7dfb2de5fb42aa2e7a79
Lazybones3 <caorui452@163.com>
2021-12-07 10:14:46 +0800

    solution 144 修改注释

--- 144.二叉树的前序遍历.py
+++ 144.二叉树的前序遍历.py
@@ -18,15 +18,13 @@ class Solution:
         cur = root
         while len(stack) > 0 or cur:
             while cur:
-                # 将当前节点的值存入结果
+                # 左移前存入结果
                 result.append(cur.val)
-                # 入栈
+                # 入栈左移
                 stack.append(cur)
-                # 访问左节点
                 cur = cur.left
-            # 出栈
+            # 出栈右移
             cur = stack.pop()
-            # 访问右节点
             cur = cur.right
         return result
 # @lc code=end
